842d4633c199ee2d9bc2cfedad476b527b9c96d6
Sam Beard <sam.beard@zelezny.nl>
2019-02-28 18:10:08 +0100

    add input output processing

--- HASHCODE_2019/HASHCODE_2019/Program.cs
+++ HASHCODE_2019/HASHCODE_2019/Program.cs
@@ -8,8 +8,19 @@ namespace HASHCODE_2019
 {
     class Program
     {
+        static int iteration = 0;
+        static string INPUT_FILE = System.IO.Directory.GetCurrentDirectory() + "/../../INPUT_FILE.txt";
+        static string OUT_FILE = System.IO.Directory.GetCurrentDirectory() + $"/../../SOLUTIONS/SOLUTION{iteration}.txt";
         static void Main(string[] args)
         {
+            string[] lines = System.IO.File.ReadAllLines(INPUT_FILE);
+            foreach (var line  in lines)
+            {
+                Console.WriteLine(line);
+            }
+            System.IO.File.WriteAllLines(OUT_FILE, lines);
+            Console.ReadLine();
+
         }
     }
 }
