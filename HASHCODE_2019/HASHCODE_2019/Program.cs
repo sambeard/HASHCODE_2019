1a8da9fc68f9e9929df95960904f7803c4ecaf24
Sam Beard <sam.beard@zelezny.nl>
2019-02-28 19:31:33 +0100

    parse input into set of tags and list of photos

--- HASHCODE_2019/HASHCODE_2019/Program.cs
+++ HASHCODE_2019/HASHCODE_2019/Program.cs
@@ -12,8 +12,8 @@ namespace HASHCODE_2019
         static string INPUT_FILE = System.IO.Directory.GetCurrentDirectory() + $"/../../INPUTS/a_example.txt";
         static string OUT_FILE = System.IO.Directory.GetCurrentDirectory() + $"/../../SOLUTIONS/SOLUTION{iteration}.txt";
         static int N;
-        static List<string> tags;
-        static List<string> photos;
+        static Dictionary<string, int> tags;
+        static List<Photo> photos;
 
         static void Main(string[] args)
         {
@@ -26,7 +26,8 @@ namespace HASHCODE_2019
         }
 
         static void parseInput(string[] lines) {
-            tags = new List<string>();
+            tags = new Dictionary<string, int>();
+            photos = new List<Photo>();
             N = int.Parse(lines[0].Split(new char[] { ' ' })[0]);
             for (int i = 0; i < N; i++)
             {
@@ -36,8 +37,20 @@ namespace HASHCODE_2019
                 string Orientation = words[0];
                 int ntgs = int.Parse(words[1]);
                 string[] ts = new string[ntgs];
+                int[] ids = new int[ntgs];
                 Array.Copy(words, 2, ts, 0, ntgs);
-                tags.AddRange(ts);
+                for (int j = 0; j < ntgs; j++)
+                {
+                    if (tags.ContainsKey(ts[j]))
+                        ids[j] = tags[ts[j]];
+                    else
+                    {
+                        tags.Add(ts[j], tags.Count);
+                        ids[j] = tags[ts[j]];
+                    }
+                        
+                }
+                photos.Add(new Photo(i, Orientation == "H", ids));
 
             }
 
