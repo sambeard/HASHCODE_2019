09ee159dffeb8281c6b32a5b636be0f5add1c9e2
Sam Beard <sam.beard@zelezny.nl>
2019-02-28 20:04:54 +0100

    add base of local search

---
+++ HASHCODE_2019/HASHCODE_2019/LocalSearch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HASHCODE_2019
+{
+    class LocalSearch
+    {
+        public Slideshow SlideShow;
+        public List<Photo> OpenPhotos;
+        public List<Photo> ClosedPhotos;
+
+        int MAX_ITERATION = 100000;
+
+        public LocalSearch(List<Photo> photos) {
+
+            OpenPhotos = photos;
+            ClosedPhotos = new List<Photo>();
+
+            // call startsolution
+
+
+        }
+
+        public void iterate() {
+            int i = 0;
+
+            while (i++ < MAX_ITERATION) {
+                // get action
+
+                // look at diff
+
+                // execute if diff > 0
+
+            }
+
+            // return slideshow
+
+        }
+
+    }
+}
