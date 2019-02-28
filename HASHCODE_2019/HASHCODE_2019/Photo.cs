5eef90e56111c26181202e3b4aab161130a4f086
Luuk <l.glorie@students.uu.nl>
2019-02-28 19:15:38 +0100

    Added photo class

---
+++ HASHCODE_2019/HASHCODE_2019/Photo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HASHCODE_2019
+{
+    class Photo
+    {
+        public int ID;
+        public bool Horizontal;
+        public int[] Tags;
+
+        public Photo(int id, bool horizontal, int[] tags)
+        {
+            ID = id;
+            Horizontal = horizontal;
+            Tags = tags;
+        }
+    }
+}
